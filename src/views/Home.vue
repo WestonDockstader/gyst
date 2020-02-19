fd7feb433ac35bf92fc58a5ee81e700a609ad3e9
Weston <dockstaderweston@gmail.com>
2020-02-19 14:39:24 -0700

    update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,29 +1,33 @@
 <template>
 
-  <div class="container-fluid home">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <img alt="orange Logo" src="../assets/circle.svg" class="img-class">
-      <a class="navbar-brand" href="#">GYST</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
-        aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav">
-          <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+  <div class="container-fluid home p-0" style="width: 100%">
+    <div class="row col-12 mb-1">
+      <img alt="orange Logo" src="../assets/ITTLogoWhite.svg" class="logo-class">
+    </div>
+    <div class="row">
+      <div class="col-1 p-3 ml-4 d-flex flex-column">
+        <div class="m-0">
+          <img alt="Generic User Logo" src="../assets/user_fill.png" class="rounded-circle profile-pic">
+          <p class="card-title mt-2 mb-0 text-white text-size">Welcome Russell Crowe</p>
+          <a href="#" class="underline text-size font-color mt-0">Sign Out</a>
+        </div>
+        <hr class="bar-color mr-1 mt-1 ml-0">
+        <div class=" p-2 m-0 d-flex align-items-start flex-column">
+          <a href="#" class="card-link m-0 text-white">Profile</a>
+          <a href="#" class="card-link m-0 text-white">Network</a>
         </div>
       </div>
-      <div class="col-sm d-flex justify-content-end">
-        <button class="btn btn-inverse btn-outline-primary m-1" data-toggle="modal"
-          data-target="#loginModal">Login</button>
-        <button class="btn btn-outline-success m-1" data-toggle="modal" data-target="#registerModal">Register</button>
+      <div class="ml-2 mr-4 card-body body">
+        <!--<img alt="Generic User Logo" src="../assets/user_fill.png" class="rounded-circle profile-pic">-->
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+          content.
+        </p>
       </div>
-    </nav>
-    <div class="row">
-
     </div>
     <login />
     <register />
+    <profile />
   </div>
   </div>
 </template>
@@ -33,13 +37,15 @@
   import Main from '@/components/Main.vue'
   import login from './Login'
   import register from './Register'
+  import profile from './Profile'
 
   export default {
     name: 'home',
     components: {
       Main,
       login,
-      register
+      register,
+      profile
     },
     data() {
       return {
@@ -52,12 +58,54 @@
 </script>
 
 <style scoped>
-  .fill {
-    height: 100%;
+  .home {
+    background: linear-gradient(135deg, #474c53 10%, #131415);
+    background-size: cover;
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
   }
 
-  .img-class {
-    width: 50px;
+  .body {
+    background-color: white;
+    background-size: cover;
+    height: 46rem;
+  }
+
+  .logo-class {
+    width: 200px;
     height: auto;
+    margin-top: 5px;
+    margin-left: 25px;
+    margin-bottom: 5px;
+  }
+
+  .profile-pic {
+    width: 100%;
+    height: auto;
+
+  }
+
+  .underline {
+    text-decoration: underline;
+    font-family: Lato;
+  }
+
+  .text-size {
+    font-size: xx-small;
+  }
+
+  .font-color {
+    color: #f26323;
+
+  }
+
+  .bar-color {
+    border-color: black;
+    display: block;
+    width: 100%;
+    height: 2px;
   }
 </style>
